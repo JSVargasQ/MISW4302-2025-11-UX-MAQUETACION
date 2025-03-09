67588904756d892f09beedaf1515b00a79037cb7
JSVargasQ <jsvargasq@hotmail.com>
2025-03-09 00:49:00 -0500

    Load login component base

---
+++ maquetacion-web/src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import {AppRoutingModule} from './app-routing.module';
+import {BrowserModule} from '@angular/platform-browser';
+
+@NgModule({
+  declarations: [
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+  ],
+})
+export class AppModule{}
---
+++ maquetacion-web/src/app/auth/login/login.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import {CommonModule} from '@angular/common';
+
+@Component({
+  selector: 'app-login',
+  imports: [CommonModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss',
+  standalone: true,
+})
+export class LoginComponent {
+
+}
